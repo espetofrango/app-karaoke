a148a32415f992827cd297f866ddb31803a10b90
Emanuel <espetofrango@gmail.com>
2026-04-17 13:20:17 -0300

    fix: correct supabase url missing char

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://jwlyhgpjfpgkopjmjwp.supabase.co'
+const supabaseUrl = 'https://jwlyhgpcjfpgkopjmjwp.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp3bHloZ3BjamZwZ2tvcGptandwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYzMDE5NzAsImV4cCI6MjA5MTg3Nzk3MH0.NC8Zf5dfEiY7YtRxh2IvzEZGG4Q0T5pNcLGzSU6YAWY'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
