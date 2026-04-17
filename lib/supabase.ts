26b19c167fc9a35d44e19d82dd4ac7a753f9a3ca
Emanuel <espetofrango@gmail.com>
2026-04-17 17:25:26 -0300

    fix: update to new supabase project credentials

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabaseUrl = 'https://jwlyhgpcjfpgkopjmjwp.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp3bHloZ3BjamZwZ2tvcGptandwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYzMDE5NzAsImV4cCI6MjA5MTg3Nzk3MH0.NC8Zf5dfEiY7YtRxh2IvzEZGG4Q0T5pNcLGzSU6YAWY'
+export const supabaseUrl = 'https://yarxcejyvtstxmqtpiux.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlhcnhjZWp5dnRzdHhtcXRwaXV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY0NTA0MDQsImV4cCI6MjA5MjAyNjQwNH0.bbHAl-tZub254gqEF7N9KWLQa26ufrXri-_qglHaM2A'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
